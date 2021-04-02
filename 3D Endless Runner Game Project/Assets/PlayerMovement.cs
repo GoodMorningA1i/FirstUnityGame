8fdef04502b443578d8596134232f0ac9a01922e
GoodMorningA1i <52847195+GoodMorningA1i@users.noreply.github.com>
2021-04-02 10:36:06 -0400

    Added fulll game to repository

---
+++ 3D Endless Runner Game Project/Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    //this is the variable that is referring to the rigidbody called "rb"
+    public Rigidbody rb; 
+
+    // FixedUpdate is called because we are using physics 
+    // to update the rigidbody's motion
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 1000 * Time.deltaTime); //Adding a force of 1000 on the z-axis
+    }
+}
